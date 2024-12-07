096694ee821b20c967e3d34ca676d6a223a76cd8
Almaz <almaz.kzn2005@yandex.ru>
2024-12-07 19:44:06 +0300

    feat: add Transformation interface

---
+++ src/main/java/backend/academy/interfaces/Transformation.java
@@ -0,0 +1,7 @@
+package backend.academy.interfaces;
+
+import backend.academy.entities.Point;
+import java.util.function.Function;
+
+public interface Transformation extends Function<Point, Point> {
+}
